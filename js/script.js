e8c55fd92fbf7f7cf838ddfc44208bd125b9216c
Trauvin <mateusgalote@gmail.com>
2021-08-17 07:50:13 -0300

    exemplo de movimento

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 const pecas = document.querySelectorAll('.lista-pecas li')
 const pecaInfo = document.querySelectorAll('.xadrez-peca-info section')
+const pecaMove = document.querySelectorAll('.move-peca div')
 
 pecaInfo[0].classList.add('ativo')
+pecaMove[0].classList.add('ativo')
 
 function ativaPeca(index) {
   pecaInfo.forEach((item) => {
     item.classList.remove('ativo')
   })
+  pecaMove.forEach((peca) => {
+    peca.classList.remove('ativo')
+  })
   pecaInfo[index].classList.add('ativo')
+  pecaMove[index].classList.add('ativo')
 }
 
 pecas.forEach((peca, index) => {
