a9505dab7bebd41e43524e1a0ddfe7ef91299d2a
Trauvin <mateusgalote@gmail.com>
2021-08-17 21:24:42 -0300

    grandes jogadores

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
-const pecas = document.querySelectorAll('.lista-pecas li')
-const pecaInfo = document.querySelectorAll('.xadrez-peca-info section')
-const pecaMove = document.querySelectorAll('.move-peca div')
+function clickShowPiece() {
 
-pecaInfo[0].classList.add('ativo')
-pecaMove[0].classList.add('ativo')
+  const pecas = document.querySelectorAll('.lista-pecas li')
+  const pecaInfo = document.querySelectorAll('.xadrez-peca-info section')
+  const pecaMove = document.querySelectorAll('.move-peca div')
 
-function ativaPeca(index) {
-  pecaInfo.forEach((item) => {
-    item.classList.remove('ativo')
+  pecaInfo[0].classList.add('ativo')
+  pecaMove[0].classList.add('ativo')
+
+  function ativaPeca(index) {
+    pecaInfo.forEach((item) => {
+      item.classList.remove('ativo')
+    })
+    pecaMove.forEach((peca) => {
+      peca.classList.remove('ativo')
+    })
+    pecaInfo[index].classList.add('ativo')
+    pecaMove[index].classList.add('ativo')
+  }
+
+  pecas.forEach((peca, index) => {
+    peca.addEventListener('click', function() {
+      ativaPeca(index)
+    })
   })
-  pecaMove.forEach((peca) => {
-    peca.classList.remove('ativo')
+}
+clickShowPiece()
+
+function infoPlayer() {
+const jogadores = document.querySelectorAll('.jogador-info dt')
+
+  jogadores[0].classList.add('ativo')
+  jogadores[0].nextElementSibling.classList.add('ativo')
+
+  jogadores.forEach((jogador) => {
+    jogador.addEventListener('click', function() {
+      jogador.classList.toggle('ativo')
+      jogador.nextElementSibling.classList.toggle('ativo')
+    })
   })
-  pecaInfo[index].classList.add('ativo')
-  pecaMove[index].classList.add('ativo')
 }
 
-pecas.forEach((peca, index) => {
-  peca.addEventListener('click', function() {
-    ativaPeca(index)
-  })
-})
+infoPlayer()
+
+
