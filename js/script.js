558ed023495a7722d533e439ec1c837f9ba592de
Trauvin <mateusgalote@gmail.com>
2021-08-19 08:08:50 -0300

    javascript suave scroll

--- js/script.js
+++ js/script.js
@@ -42,4 +42,24 @@ function infoPlayer() {
 
 infoPlayer()
 
+function scrool() {
 
+  const linksInternos = document.querySelectorAll('.menu-nav a[href^="#"')
+
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection)
+  })
+}
+
+scrool()
